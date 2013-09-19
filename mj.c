fdef8e177a01ef357f926ff243726d37c7ab1ced
tangentstorm <michal.wallace@gmail.com>
2013-09-19 13:15:00 -0500

    implemented verbs (id and nop)

--- mj.c
+++ mj.c
@@ -17,28 +17,50 @@ A ar(I t, r, D) { A z=(A)ma(5+tr(r,d));z->t=t,z->r=r,mv(z->d,d,r);R z;}
 // print
 _ nl(         ) { P("\n");}
 _ pi(  i      ) { P("%d ",i);}
-_ pr(A w      ) { I r=w->r,*d=w->d,n=tr(r,d); DO(r,pi(d[i]))}
+_ pr(A w      ) { I r=w->r,*d=w->d,n=tr(r,d);
+                    //P("pr> printing %ld values.\n", n);
+                    DO(r,pi(d[i]))         // print dimensions if rank>0
+                    DO(n, pi(w->p[i]));    // print values
+                    nl();}
+
+// verbs
+#define MO(fn) A fn(A w)
+#define DY(fn) A fn(A a, A w)
+MO(  id){ P("id.\n");  R w;} // id function
+DY( nop){ P("nop.\n"); R w;} // no-op (J: "]")
 
 // eval
 A st[26];                                     // storage for pronouns
-I qp(I a      ) { R a>='a'&&a<='z';}          // is it a pronoun?
-I qv(I a      ) { R a<'a';}                   // is it a verb?
-A ev(I*e      ) { A a=(A)*e; C c=*e;
-                    if(qp(c)){                // eval pronouns:
-                        if(e[1]=='=') R st[c-'a']=ev(e+2);   // set
-                        a=st[c-'a'];}                        // get
-                  R a; }
+C vt[]="+{~<#,";                                  // verb table
+A (*mo[])()={ id,  id,  id,  id,  id,  id,  id},  // monadic
+  (*dy[])()={nop, nop, nop, nop, nop, nop, nop};  // dyadic
+
+I qp(I a) { R a>='a'&&a<='z';}          // is it a pronoun?
+I qv(I a) { R a<'a';}                   // is it a verb?
+A ev(I*e) { I a=*e;
+            if(qp(a)){                  // P("ev> pronoun: %c\n", (char)a);
+              if(e[1]=='=')
+		R st[a-'a']=ev(e+2);    // set
+	      a=(I)st[a-'a'];}          // get
+            // -- right-to-left recursive evaluator
+	    R (qv(a)) ? (*mo[a])(ev(e+1))
+            : (e[1])  ? (*dy[e[1]])(a,ev(e+2))
+            :           (A)a; }
 
 // read (nouns and verbs)
 I nn(C c      ) { A z;if(c<'0'||c>'9') R 0;   // nn only handles 0..9
-                    z=ar(0,0,0);*z->p=c-'0';  // create rank 0 array
-                    R (I)z;}                  // cast A->I and return
-I vb(C c      ) { R 0;}
+                    z=ar(0,0,0); *z->p=c-'0'; // create rank 0 array
+		    //P("nn> noun: %ld\n", *z->p);
+                  R (I)z;}                    // cast A->I and return
+I vb(C c      ) { I i=0;for(;vt[i];)if(vt[i++]==c)R i;R 0;}
 I*rd(C*s      ) { I a,n=strlen(s),*e=ma(n+1); C c;
                     DO(n,e[i]=(a=nn(c=s[i]))?a // noun? (single digit)
                     :(a=vb(c))?a               // verb? (see vt[])
                     :c);                       // else pronoun (a..z)
-                  e[n]=0;
+                    e[n-1]=0;                  // strip linefeed
+                    //P("rd> n: %ld\n", n);
+                    //DO(n, P("rd> i: %ld, e[i]: %ld\n", i, e[i]));
                   R e;}
 #define sz 99
-main() {C s[sz];while(fgets(s,sz,stdin))pr(ev(rd(s)));}
+main() {C s[sz]; DO(26,st[i]=ar(0,0,0))
+        while(fgets(s,sz,stdin))pr(ev(rd(s)));}
