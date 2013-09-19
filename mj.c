cf4f6e500a4d98886bb6f9454e5965e2b087b6cc
tangentstorm <michal.wallace@gmail.com>
2013-09-19 10:24:09 -0500

    typedef void _ instead of typedef void V

--- mj.c
+++ mj.c
@@ -1,19 +1,19 @@
 #import <stdio.h>
 #import <stdlib.h>
 #import <string.h>
-typedef char C; typedef long I; typedef void V;
+typedef char C; typedef long I; typedef void _;
 typedef struct a{I t,r,d[3],p[2];}* A;
 #define P printf
 #define R return
 #define DO(n,x){I i=0,_n=(n);for(;i<_n;++i){x;}}
 
 I*ma(I n     ) {R(I*)malloc(n*4);}
-V mv(I*d,*s,n) {DO(n,d[i]=s[i]);}
+_ mv(I*d,*s,n) {DO(n,d[i]=s[i]);}
 I tr(I   r,*d) {I z=1;DO(r,z=z*d[i]);R z;} // total number of cells.
 A ar(I t,r,*d) {A z=(A)ma(5+tr(r,d));z->t=t,z->r=r,mv(z->d,d,r);R z;}
-V nl(        ) {P("\n");}
-V pi(  i     ) {P("%d ",i);}
-V pr(A w     ) {pi(0);nl();}
+_ nl(        ) {P("\n");}
+_ pi(  i     ) {P("%d ",i);}
+_ pr(A w     ) {pi(0);nl();}
 A ev(I*e     ) {R(A)e;}
 I nn(C c     ) {A z;if(c<'0'||c>'9')R 0;z=ar(0,0,0);*z->p=c-'0';R(I)z;}
 I vb(C c     ) {R 0;}
