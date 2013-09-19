913a3c74831c0d2aaafac526e1d24102066fac89
tangentstorm <michal.wallace@gmail.com>
2013-09-19 07:06:18 -0500

    Rough sketch of a rd/ev/pr loop. (ev(any)==0).
    I created an empty typedef for arrays (A) just so all the
    types would work out.

--- mj.c
+++ mj.c
@@ -1,5 +1,12 @@
 #import <stdio.h>
-typedef char C;
+typedef char C; typedef long I; typedef void V;
+typedef struct a{}* A;
 #define P printf
+#define R return
+V nl(   ){P("\n");}
+V pi(  i){P("%d ",i);}
+V pr(A w){pi(0);nl();}
+A ev(I*e){R(A)e;}
+I*rd(C*s){R 0;}
 #define sz 80
-main() {C s[sz];while(fgets(s,sz,stdin))P("%s",s);}
+main(){C s[sz];while(fgets(s,sz,stdin))pr(ev(rd(s)));}
